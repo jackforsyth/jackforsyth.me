b9205107542ec4c8c2075ad145fcc7314bbad40c
jackforsyth <jackforsyth2004@gmail.com>
2024-09-07 10:53:49 +1000

    Update page.tsx
    Hiding blog section for now.

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Jack Forsyth
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        I'm a student studying economics and Entrepreneurship at RMIT university.
       </p>
       <div className="my-8">
-        <BlogPosts />
+{/*         <BlogPosts /> */}
       </div>
     </section>
   )
